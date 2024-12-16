aa147d659cc01fcca74a26beb3f26446a847b9a4
Nikki Cupples <ncupples03@gmail.com>
2024-12-16 22:38:05 +1300

    change subtitle

--- client/components/Home/Header.tsx
+++ client/components/Home/Header.tsx
@@ -2,7 +2,7 @@ function Header() {
   return (
     <header className="flex flex-col items-center w-full rounded-b-lg bg-black p-6 pt-16 text-pink-500 animate-slide-in shadow-md">
       <h1 className="text-6xl font-bold">Snake</h1>
-      <p className="mt-2 italic text-xl text-pink-500">By Nikki Cupples</p>
+      <p className="mt-2 italic text-xl text-pink-500">pssst! how many blocks can you eat?</p>
     </header>
   )
 }
