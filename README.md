a3045d96420600bf2bd6e084875b30b69b752546
Nikki Cupples <ncupples03@gmail.com>
2024-12-16 19:18:32 +1300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Nikki's Full Stack Template
+Complete setup for full stack react apps
---
+++ client/components/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <div className="app">My App</div>
+    </>
+  )
+}
+
+export default App
